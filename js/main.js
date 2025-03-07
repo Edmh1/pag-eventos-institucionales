0adff2387241d8215417928441f4e46194f38130
SebastianCA19 <castroarrietasebastiandavid@gmail.com>
2025-03-07 09:38:01 -0500

    feat: implement user authentication modal and update header with user options

--- js/main.js
+++ js/main.js
@@ -1,14 +1,66 @@
-document.addEventListener("DOMContentLoaded", function () {
-    
-    // Botón "Ingresar" en el header
+function openModal(){
     document.getElementById("ingresar").addEventListener("click", function (event) {
-        event.preventDefault(); 
-        document.getElementById("modal").style.display = "flex"; // Muestra el modal
+        event.preventDefault();
+        document.getElementById("modal").style.display = "flex"; 
     });
-    
 
-    // Botón "cerrar" dentro del modal
     document.querySelector(".close").addEventListener("click", function () {
-        document.getElementById("modal").style.display = "none"; // Oculta el modal
+        document.getElementById("modal").style.display = "none"; 
     });
-});
+}
+
+function changeUserType() {
+    usuario = document.getElementById("user").value;
+    password = document.getElementById("password").value;
+    if (usuario == "user" && password == "user") {
+        localStorage.setItem("userType", "user");
+    }
+
+    if (usuario == "admin" && password == "admin") {
+        localStorage.setItem("userType", "admin");
+    }
+
+    updateHeader();
+}
+
+function updateHeader() {
+    const header = document.querySelector("header");
+    const userType = localStorage.getItem("userType") || "guest";
+    let contenidoHeader = `
+    <div id="cont-logo">
+        <img id="logo" src="resources/img/logo-umagdalena.svg" alt="Logo Institucional">
+    </div>
+    <div id="cont-nombre-institucion">
+        <h2 id="nombre-institucion">UNIVERSIDAD DEL MAGDALENA</h2>
+    </div>
+    `;
+
+    if (userType === "guest") {
+        contenidoHeader += `
+        <div id="cont-ingresar">
+            <button id="ingresar" class="button-header" onclick="openModal()">Ingresar</button>
+        </div>
+    `;
+    } else {
+        contenidoHeader += `
+        <div id="cont-user" class="user-header">
+            <button id="mis-eventos" class="button-header">Mis Eventos</button>
+    `;
+
+        if (userType === "admin") {
+            contenidoHeader += `<button id="crear-evento" class="button-header">Crear Evento</button>`;
+        }
+
+        contenidoHeader += `<button id="cerrar-sesion" class="button-header" onclick="resetUserType()">Cerrar Sesión</button>
+                            <img src="./resources/img/user-solid.svg" alt="Foto de perfil" id="user-pic">
+                            </div>`;
+        document.getElementById("modal").style.display = "none";
+    }
+
+    header.innerHTML = contenidoHeader;
+}
+
+function resetUserType() {
+    localStorage.removeItem("userType");
+    updateHeader();
+}
