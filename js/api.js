64c3c7dfeacef3da7a4f2e62695b5758c5e8b88b
Edmh <edavidmanotas@gmail.com>
2025-04-09 07:06:34 -0500

    fix: update API base URL to include versioning and improve API URL structure

--- js/api.js
+++ js/api.js
@@ -1,5 +1,11 @@
-const API_BASE_URL = "http://localhost:8080";
-const API_USER = API_BASE_URL + "/usuarios";
+const PRO = false; // true = TOMCAT EXTERNO || false = TOMCAT EMBEBIDO
+
+const API_BASE_URL = "http://localhost:8080/pag-eventos-institucionales-back-0.0.1-SNAPSHOT";
+const API_BASE_URL_PRUEBA = "http://localhost:8080";
+
+const API_URL = PRO ? API_BASE_URL : API_BASE_URL_PRUEBA;
+
+const API_USER = API_URL + "/usuarios";
 const API_USER_LOGIN = API_USER + "/login";
 const API_USER_REGISTER = API_USER + "/registro";
 
